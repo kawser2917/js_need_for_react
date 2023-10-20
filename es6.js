840282548ccb4798cfebf5235af7f5b86d400352
kawser2917 <kawserahmed8492@gmail.com>
2023-10-20 17:42:49 +0600

    arrow function

--- es6.js
+++ es6.js
@@ -9,3 +9,21 @@ const students ={
 
 const about = `My name is ${students.name} age of ${students.age} number of ${numbers[5]} also like movie ${students.movie[0]}`
 console.log(about)
+
+// Arrow function 
+// Single line this will directly return
+const number = () => 55;
+const isEven = x => x % 2 === 0;
+const addNumber = (x,y,z=5) => x+y+z;
+
+// Multiline arrow function
+// This will not return directly we need to write return
+const doMath = (num1,num2,num3) => {
+    const sum = num1 + num2 + num3;
+    return sum;
+}
+
+console.log(number())
+console.log(isEven(20))
+console.log(addNumber(10,20))
+console.log(doMath(54,52,36))
